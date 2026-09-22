52c63b1067bf482f55e3186ab6fd86f271c81309
ankushchoudhary9 <ankushchoudhary919@gmail.com>
2026-09-22 22:57:07 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,3 @@
-chmod +x scripts/*.sh
-git update-index --chmod=+x scripts/*.sh
-git add . && git commit -m "fix scripts" && git push
+#!/bin/bash
+echo "<p>Served by: $(hostname)</p>" >> /var/www/html/index.html
+systemctl restart httpd
